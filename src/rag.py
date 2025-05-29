7cb78412854ea563eaf65c000d3ec0b68c7e7059
Pablo Sanchez Orozco <sanchezpablo95up@gmail.com>
2025-05-29 02:57:00 +0000

    On branch ai_model  Changes to be committed: 	modified:   .gitignore 	new file:   src/playground.ipynb 	modified:   src/rag.py
    .gitignore: Adding .vscode and src/model
    
    playground: Creating the RAG logic... still needs cleanup.
    
    rag: Setting the flow for main script

--- src/rag.py
+++ src/rag.py
@@ -1,11 +1,12 @@
+import os
 from modules import chunker, indexer
 
 
 def main():
     
     #Creates text chunk based on tokens for processing
-    docs = chunker.document_chunker(directory_path='/home/paborsan/Documents/RAG_Data',
-                            model_name='BAAI/bge-small-en-v1.5',
+    docs = chunker.document_chunker(directory_path=os.path.expanduser(),
+                            model_name='mistralai/Mistral-7B-Instruct-v0.2',
                             chunk_size=256)
     
     vec_store = indexer.create_vector_store(docs)
