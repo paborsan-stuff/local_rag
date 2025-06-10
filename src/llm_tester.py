f72fcfd9f7dab30a1e349b644fe886a0d96e73d2
aafs <afiguerog@gmail.com>
2025-06-09 21:55:43 -0600

    LLM assistant running

--- src/llm_tester.py
+++ src/llm_tester.py
@@ -15,10 +15,23 @@ retrieved_docs = """
 Wall-mounted electric fireplace with realistic LED flames and heat settings. Features a black glass frame and remote control for easy operation. Ideal for adding warmth and ambiance. Manufactured by Hearth & Home. Dimensions: 50"W x 6"D x 21"H.
 """
 
-prompt = construct_prompt(system_prompt=system_prompt,
+
+llm = Llama(model_path="tmp/llm/mistral-7b-instruct-v0.2.Q3_K_L.gguf", n_gpu_layers=1)
+instance_model = LLMAdapter(llm)
+
+llm_prompt = LLMAdapter.construct_prompt(system_prompt=system_prompt,
                           retrieved_docs=retrieved_docs,
-                          user_query="I am looking for a wall-mounted electric fireplace with realistic LED flames")
+                          user_query="Hola explicame que es un hogar de cine en casa y como se usa")
 
-llm = Llama(model_path="/Users/joesasson/Downloads/mistral-7b-instruct-v0.2.Q3_K_L.gguf", n_gpu_layers=1)
+response = instance_model.stream_and_buffer_response(base_prompt = llm_prompt, max_tokens = 800)
 
-stream_and_buffer(prompt, llm)
+print (response)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        model_path = sys.argv[1]
+        llm = Llama(model_path=model_path, n_gpu_layers=1)
+        instance_model = LLMAdapter(llm)
+        print(f"LLM model loaded from {model_path}")
+    else:
+        print("Usage: python llm_tester.py <path_to_llm_model>")
